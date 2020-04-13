77f60a3483a64fd12f2bf0662f1d34280686f56c
jeff wu <j98728@hotmail.com>
2020-04-13 09:24:47 +0100

    Used props DI for restdb

--- src/main/java/jw/io/web/RestDbClient.java
+++ src/main/java/jw/io/web/RestDbClient.java
@@ -3,13 +3,24 @@ package jw.io.web;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
+@Component
+@PropertySource("classpath:restdbprops.properties")
 public class RestDbClient implements Rest {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Value("${data.endpoint}")
+    private String dataEndpoint;
 
     @Override
     public String get() throws UnirestException {
-        HttpResponse<String> response = Unirest.get("https://cv19mars-9e51.restdb.io/rest/case-data-model")
-                .header("x-apikey", "*****************************")
+        HttpResponse<String> response = Unirest.get(dataEndpoint)
+                .header("x-apikey", apiKey)
                 .header("cache-control", "no-cache")
                 .asString();
         return response.getBody();
